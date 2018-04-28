385c978b6d3d06534f6e8cf12a38ebed7b8b6571
htet-waiyan <h.waeyan@gmail.com>
2018-04-28 18:53:02 +0800

    re-structure the project folder to add test

--- src/route/route.api.js
+++ src/route/route.api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/echo',(req,res,next) => {
     logger.info('Returning echo');
-    res.status(200).json({status: 'success',message: 'Hellow from Version Control'})
+    res.status(200).json({status: 'success',message: 'Hello from Version Control'})
 })
 
 module.exports = router;
